44e8b334db8d5c2d08886117f0e766cab1de3a3f
GeisonRDS <geisonrobertodasilva@gmail.com>
2020-07-26 02:37:37 -0300

    Created home layout

--- lib/main.dart
+++ lib/main.dart
@@ -15,17 +15,82 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Calculadora de IMC'),
-        centerTitle: true,
+        appBar: AppBar(
+          title: Text('Calculadora de IMC'),
+          centerTitle: true,
+          backgroundColor: Colors.green,
+          elevation: 0.0,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: () {},
+            )
+          ],
+        ),
         backgroundColor: Colors.green,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.refresh),
-            onPressed: () {},
-          )
-        ],
-      ),
-    );
+        body: SingleChildScrollView(
+          padding: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Icon(
+                Icons.person_outline,
+                size: 120.0,
+                color: Colors.white,
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Peso (kg)',
+                  labelStyle: TextStyle(color: Colors.white),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                ),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white, fontSize: 25.0),
+                cursorColor: Colors.white,
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Altura (cm)',
+                  labelStyle: TextStyle(color: Colors.white),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                ),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white, fontSize: 25.0),
+                cursorColor: Colors.white,
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 20.0),
+                child: Container(
+                  height: 60.0,
+                  child: RaisedButton(
+                    onPressed: () {},
+                    child: Text(
+                      'CALCULAR',
+                      style: TextStyle(color: Colors.green, fontSize: 25.0),
+                    ),
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Text(
+                'Info',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white, fontSize: 25.0),
+              )
+            ],
+          ),
+        ));
   }
 }
